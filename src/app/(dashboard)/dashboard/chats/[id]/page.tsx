a1a845eb90eda7cb653630ed92fe233ba8d612e7
OP <106511969+Op-Pautu@users.noreply.github.com>
2024-11-10 17:00:01 +0530

    add basic structure of chat page UI

--- src/app/(dashboard)/dashboard/chats/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/chats/[id]/page.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 
 const ChatPage = () => {
-	return <div>ChatPage</div>;
+	const endRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (endRef.current) {
+			endRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	}, []);
+	return (
+		<div className="h-full flex flex-col items-center">
+			<div className="flex-1 overflow-scroll w-full flex justify-center">
+				<div className="w-1/2 flex flex-col">
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from user
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from usuer
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from user
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from usuer
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from user
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from usuer
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from user
+					</div>
+					<div className="p-5">Text message from ai</div>
+					<div className="p-5 bg-[#2c2937] rounded-[20px] max-w-[80%] self-end">
+						Text message from usuer
+					</div>
+					<div ref={endRef}></div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ChatPage;
